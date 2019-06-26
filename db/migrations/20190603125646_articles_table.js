e263828ccc658487aa8cb0190b4a52abc50949b2
Ben Shearer <benshearer@Bens-MacBook-Pro.local>
2019-06-26 16:59:12 +0100

    Made all votes possitive so the vote feature can become a like feature

--- db/migrations/20190603125646_articles_table.js
+++ db/migrations/20190603125646_articles_table.js
@@ -1,17 +1,25 @@
-
 exports.up = function(knex, Promise) {
-    ('creating articles table...');
-    return knex.schema.createTable('articles', (articlesTable) => {
-      articlesTable.increments('article_id').primary();
-      articlesTable.string('title').notNullable();
-      articlesTable.integer('votes').defaultTo(0);
-      articlesTable.string('topic').references('topics.slug').notNullable();
-      articlesTable.string('author').references('users.username').notNullable();
-      articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
-      articlesTable.text('body').notNullable();
-    });
+  ("creating articles table...");
+  return knex.schema.createTable("articles", articlesTable => {
+    articlesTable.increments("article_id").primary();
+    articlesTable.string("title").notNullable();
+    articlesTable
+      .integer("votes")
+      .unsigned()
+      .defaultTo(0);
+    articlesTable
+      .string("topic")
+      .references("topics.slug")
+      .notNullable();
+    articlesTable
+      .string("author")
+      .references("users.username")
+      .notNullable();
+    articlesTable.timestamp("created_at").defaultTo(knex.fn.now());
+    articlesTable.text("body").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('articles');
+  return knex.schema.dropTable("articles");
 };
--- db/migrations/20190603142844_comment_table.js
+++ db/migrations/20190603142844_comment_table.js
@@ -1,15 +1,23 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('comments', (commentsTable) => {
-      commentsTable.increments('comment_id').primary();
-      commentsTable.string('author').references('users.username').notNullable();
-      commentsTable.integer('article_id').references('articles.article_id').notNullable();
-      commentsTable.integer('votes').defaultTo(0);
-      commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
-      commentsTable.text('body').notNullable()
-    });
+  return knex.schema.createTable("comments", commentsTable => {
+    commentsTable.increments("comment_id").primary();
+    commentsTable
+      .string("author")
+      .references("users.username")
+      .notNullable();
+    commentsTable
+      .integer("article_id")
+      .references("articles.article_id")
+      .notNullable();
+    commentsTable
+      .integer("votes")
+      .unsigned()
+      .defaultTo(0);
+    commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
+    commentsTable.text("body").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('comments');
+  return knex.schema.dropTable("comments");
 };
